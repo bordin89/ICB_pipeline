3e4cf905b7dbe726b4fbc1adb1c44d75050a4835
Nicola Bordin <bordin89@gmail.com>
2016-04-21 17:24:58 +0200

    Added files via upload

---
+++ make_tsv_table.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import re,sys,os
+import gzip
+from collections import defaultdict
+
+Data=defaultdict(str)
+
+i=0
+infile=gzip.open( sys.argv[1] )
+for line in infile:
+	Data[i]=line.rstrip()
+	i+=1
+infile.close()
+
+for file in (sys.argv[2:]):
+	infile=gzip.open(file)
+	i=0
+	for line in infile:
+		info = line.split("\t",1)[1].rstrip()
+		#info="".join(re.split("\s+", line.rstrip())[1:])
+		Data[i]+="\t"+info
+		i+=1
+	infile.close()
+
+for i in Data:
+	print Data[i]
+			
---
+++ tsv_to_html.pl
@@ -0,0 +1,101 @@
+#usr/bin/perl
+
+# Creates a HTML table from a TSV file
+
+use warnings;
+use strict;
+
+my $tsv_input 	= $ARGV[0]."_table.tsv";
+my $html_output = $ARGV[0]."_table.html";
+my $fasta 	= $ARGV[0]."_proteins.fa";
+my $table_name 	= $ARGV[0]; $table_name =~ s/\_/ /g;
+
+#print "$tsv_input\n";##
+#print "$html_output\n";
+#print "$fasta\n"; ##
+#print "$table_name\n"; ##
+
+
+my @column_names;
+my $i=0;
+
+open (TSV, $tsv_input) or die $!;
+while(<TSV>){}; seek TSV,0,0;
+my $proteins = $. - 1;
+
+
+open (STDOUT, ">$html_output") or die $!;
+
+
+print <<END;
+<html>
+
+<head>
+	<link rel="stylesheet" href="table_style.css">
+</head>
+END
+
+print 	"<address>By <b>Juan Carlos Gonzalez Sanchez</b> (juancarlosgonzs\@gmail.com) and <b>Damien P. Devos</b> (dpdev\@upo.es)<br>\n",
+		"Centro Andaluz de Biologia del Desarrollo. Universidad Pablo de Olavide. Sevilla<br>\n",
+		"1 May 2015</address>\n",
+		
+		"<h1>$table_name ($proteins proteins)</h1>\n\n",
+		
+		"<p>The proteins sequeneces in FASTA format used in this analysis can be downloaded\n",
+		"<a href=\"$fasta\" download>here</a>\n",
+		".</p>\n",
+		"<p>The complete table is also available in \n", 
+		"<a href=\"$tsv_input\" download>.tsv</a>\n", 
+		"format for downloading.</p>\n",
+		
+		"<table>\n";
+
+#chomp ($_ = <STDIN>);
+#s?\t?</th><th>?g;
+
+
+# Read input TSV file and print content in rows 
+while (my $line = <TSV>) {
+	$i++;
+	
+	# Print column names  ~ parses them from the file 1st row.
+	if ($i == 1) { 
+		@column_names = split ('\t', $line);
+		print "<tr>\n";
+		foreach my $name (@column_names) {	
+			
+			# Setting specific witdths
+#			if ($name =~ /Protein Length/) { print "<th id=\"length\">";}	
+			
+#			else { 
+
+			print "<th>";
+			print "$name</th>\n";
+		}
+		print "</tr>\n<tbody>\n";
+		
+	} else {
+		print "<tr>\n";
+		my @fields = split('\t', $line);
+		
+		for my $cell(@fields) {
+	
+	# The fuck is this?
+	#		if($cell =~ /^(\d|\s)+$/) {
+	#			print "<td align=\"right\">$cell</td>";
+	#		} else {
+				print "<td>$cell</td>";
+	#		}
+		}
+		print "</tr>\n"; 
+	}
+}
+
+print <<END
+</tr>
+
+</table>
+</tbody>
+
+</html>
+END
